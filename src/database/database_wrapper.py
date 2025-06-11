d082b47bfb1b81153d99bbc90e5e799a8b526056
Richard Chan <r32095@gmail.com>
2025-06-11 00:11:38 -0500

    add tiny db and classes and basic endpoint implementation

---
+++ src/database/database_wrapper.py
@@ -0,0 +1,49 @@
+from tinydb import TinyDB, Query
+from dataclasses import asdict
+from src.database.entities import *
+import logging
+
+logger = logging.getLogger(__name__)
+
+db = TinyDB("db.json")
+players = db.table("players")
+games = db.table("games")
+refs = db.table("player_game_ref")
+
+PlayerQuery = Query()
+GameQuery = Query()
+PlayerGameRefQuery = Query()
+
+
+def read_player(steam_id: str):
+    logger.info("finding player")
+    return players.search(PlayerQuery.steam_id == steam_id)
+
+
+def add_player(player: Player):
+    logger.info("inserting new player")
+    existing_player = read_player(player.steam_id)
+    if existing_player:
+        logger.info("existing player found")
+        return
+
+    p = asdict(player)
+    players.insert(p)
+
+
+def add_game(game: Game):
+    logger.info("inserting new game: %s", game.name)
+    games.insert(asdict(game))
+
+
+def add_player_game_ref(player_game_ref: PlayerGameRef):
+    logger.info("inserting reference")
+    refs.insert(asdict(player_game_ref))
+
+
+def get_all_players():
+    return [Player(**player).steam_id for player in players.all()]
+
+
+def clean_player_games(steam_id: str):
+    refs.remove(PlayerGameRefQuery.steam_id == steam_id)
--- src/steam/steam_api_wrapper.py
+++ src/steam/steam_api_wrapper.py
@@ -3,14 +3,13 @@ from requests import get
 from src.lib.exceptions import SteamApiException
 
 
-def get_owned_games():
+def get_owned_games(steam_id):
     steam_api_key = getenv("STEAM_API_KEY")
-    steam_id = getenv("STEAM_ID")
     url = f"http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key={steam_api_key}&steamid={steam_id}&include_appinfo=1&format=json"
     response = get(url)
     if response.status_code == 200:
         result = response.json()
         games = result["response"]["games"]
-        return [game["name"] for game in games]
+        return [(game["appid"], game["name"]) for game in games]
     else:
         raise SteamApiException(f"error occurred with api: {response}")
